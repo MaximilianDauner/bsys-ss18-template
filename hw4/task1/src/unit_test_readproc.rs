341d13c853cb04f71090a4974717c489d429256c
Michael Mächtel <maechtel@htwg-konstanz.de>
2018-04-09 10:59:22 +0200

    rustfmt

--- hw4/task1/src/unit_test_readproc.rs
+++ hw4/task1/src/unit_test_readproc.rs
@@ -3,7 +3,6 @@ mod tests {
     use procinfo::pid::{status, status_self};
     use {get_ownprocess_mem, get_pid_command, get_task_total, get_thread_count, self_pids};
 
-
     fn sol_self_pids() -> (i32, i32) {
         match status_self() {
             Ok(status) => (status.pid, status.ppid),
@@ -38,7 +37,6 @@ mod tests {
     fn test2_command() {
         assert_eq!(Ok(name_of_init()), get_pid_command(1));
     }
-
 
     #[test]
     fn test3_systemd_threads() {
